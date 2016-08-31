c27ad1dbc90ba437f5449f2cd6baf1bada5fbdfb
Salvador Molina <inthorinrain@gmail.com>
2016-08-31 18:16:45 +0200

    Initial commit: Application skeleton

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * Main application js file.
+ */
+
+// When window is ready, initialise app.
+window.onload = init;
+
+/**
+ * Main entry point for the application.
+ */
+function init() {
+  console.log("Initialising application");
+}
