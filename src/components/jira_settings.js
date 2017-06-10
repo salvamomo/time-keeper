4e8db1a844084924e2fee4465ad6fa1cc117f4ad
Salvador Molina <inthorinrain@gmail.com>
2017-06-10 22:11:35 +0200

    Added missing value from localStorage for Jira backend

--- src/components/jira_settings.js
+++ src/components/jira_settings.js
@@ -6,6 +6,7 @@ window.onload = init;
 function init() {
   // App accessible from nw.App;
   // Load existing values.
+  document.getElementsByName('jira_url').item(0).value = window.localStorage.getItem('jira_url');
   document.getElementsByName('jira_username').item(0).value = window.localStorage.getItem('jira_username');
   document.getElementsByName('jira_password').item(0).value = window.localStorage.getItem('jira_password');
 
